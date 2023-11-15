a87df13e7529bd8b28f7e4bc159cfc537a02571c
zhanyil2 <zl369@duke.edu>
2023-11-15 17:22:57 -0500

    first_zhanyi_push

---
+++ chat-app-rust/target/debug/deps/chat_app-4f8047b3cebda8d7.d
@@ -0,0 +1,7 @@
+/Users/zhanyilin/Documents/GitHub/rust-chat-app-Scott/chat-app-rust/target/debug/deps/chat_app-4f8047b3cebda8d7.rmeta: src/lib.rs
+
+/Users/zhanyilin/Documents/GitHub/rust-chat-app-Scott/chat-app-rust/target/debug/deps/chat_app-4f8047b3cebda8d7.d: src/lib.rs
+
+src/lib.rs:
+
+# env-dep:CARGO_MANIFEST_DIR=/Users/zhanyilin/Documents/GitHub/rust-chat-app-Scott/chat-app-rust
---
+++ chat-app-rust/target/debug/deps/chat_app-f8f4e488b1587710.d
@@ -0,0 +1,7 @@
+/Users/zhanyilin/Documents/GitHub/rust-chat-app-Scott/chat-app-rust/target/debug/deps/chat_app-f8f4e488b1587710.rmeta: src/lib.rs
+
+/Users/zhanyilin/Documents/GitHub/rust-chat-app-Scott/chat-app-rust/target/debug/deps/chat_app-f8f4e488b1587710.d: src/lib.rs
+
+src/lib.rs:
+
+# env-dep:CARGO_MANIFEST_DIR=/Users/zhanyilin/Documents/GitHub/rust-chat-app-Scott/chat-app-rust
